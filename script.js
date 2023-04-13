0bc952089857af74ee35993ce44f69b1d3b337a8
Rifresh <94816676+ergolovko@users.noreply.github.com>
2023-04-13 17:31:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 function alerted(){
   alert("Вы зарегестрированны ");
 }
+
+$(function() {
+  $('#role').change(function () {
+      var vibor = $(this).find(': selected').val();
+      if(vibor == '"user') {
+        $('.element').text('Контент 1');
+      } else if(vibor == 'executor') {
+        $('.element').text('Контент 2');
+      } else if(vibor == 'manager') {
+        $('.element').text('Контент 3');
+      } 
+  });
+
+});
